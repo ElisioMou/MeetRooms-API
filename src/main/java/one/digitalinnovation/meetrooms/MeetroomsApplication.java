ea73c66422717f507f7d972092d49565907e83ec
Elisio Elis de Moura <elisiomou@gmail.com>
2020-12-06 21:55:06 -0300

    Descompactando e criando os arquivos do projeto Angular-Spring

---
+++ src/main/java/one/digitalinnovation/meetrooms/MeetroomsApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.meetrooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetroomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetroomsApplication.class, args);
+	}
+
+}
